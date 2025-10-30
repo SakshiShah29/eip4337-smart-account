420dac4cd7fe7126591f500a7716c3ac61853ce2
SakshiShah29 <sakshi.shah299@gmail.com>
2025-10-30 12:33:16 +0530

    feat:Added EIP-191 signature validation with messageHashUtils and ECDSA.recover()

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -5,23 +5,50 @@ pragma solidity ^0.8.18;
 import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
 import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "lib/account-abstraction/contracts/core/Helpers.sol";
 
-contract MinimalAccount is IAccount,Ownable {
+contract MinimalAccount is IAccount, Ownable {
+    error MissingFundsPayer();
 
-constructor(address initialOwner) Ownable(initialOwner) {
-    }
+    constructor(address initialOwner) Ownable(initialOwner) {}
 
     function validateUserOp(
         PackedUserOperation calldata userOp,
         bytes32 userOpHash,
         uint256 missingAccountFunds
-    ) external returns (uint256 validationData){
-        // _validateSignature(userOp, userOpHash);
+    ) external returns (uint256 validationData) {
+        validationData = _validateSignature(userOp, userOpHash);
+        //Here if you require you can do some nonce validatio n check
+        // Now we require to payback to the entrypoint or the paymaster the rest of the funds
+        _paymasterOrEntrypoint(missingAccountFunds);
     }
 
+    //The userOpHash is in the EIP-191 FORMAT
     function _validateSignature(
         PackedUserOperation calldata userOp,
         bytes32 userOpHash
     ) internal view returns (uint256 validationData) {
+        bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(
+            userOpHash
+        );
+        address signer = ECDSA.recover(ethSignedMessageHash, userOp.signature);
+        if (signer != owner()) {
+            return SIG_VALIDATION_FAILED;
+        }
+        return SIG_VALIDATION_SUCCESS;
+    }
+
+    function _paymasterOrEntrypoint(uint256 missingAccountFunds) internal {
+        if (missingAccountFunds > 0) {
+            (bool success, ) = payable(msg.sender).call{
+                value: missingAccountFunds,
+                gas: type(uint256).max
+            }("");
+            if (!success) {
+                revert MissingFundsPayer();
+            }
+        }
     }
 }
